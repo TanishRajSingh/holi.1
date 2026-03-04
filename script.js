5b8223f6e7542c8bc8d65f565562031f0af9ea63
TanishRajSingh <tanishrajsinghrajput07@gmail.com>
2026-03-04 09:02:00 +0530

    hi i am live

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const card = document.getElementById("glassCard");
+
+    // Smooth appearance after small delay
+    setTimeout(function(){
+        card.classList.add("show");
+    }, 500);
+
+});
